d122e5ea904858de4a4cf5cf0427fb7e1ce69255
Roy-Gal-Git <Self.roygal@gmail.com>
2021-12-07 08:47:44 +0200

    Wrap the try-except block as a dynamic function

--- server.py
+++ server.py
@@ -9,16 +9,17 @@ table = [
     [0, 0, 0, 0, 0, 0, 0]
 ]
 
-check = [checkWinDown, checkWinSides, checkWinDiagNEtoSW, checkWinDiagSEtoNW]
+checkWinFuncs = [checkWinDown, checkWinSides, checkWinDiagNEtoSW, checkWinDiagSEtoNW]
 
-try:
-    for func in check:
-        if func(table, 1, 8, 1):
-            break
-except IndexError as e:
-    print(f'[ERROR] {e}')
 
-# checkWinDiagSEtoNW(table, 1, 4, 1)
-# checkWinDiagNEtoSW(table, 1, 4, 1)
-# checkWinSides(table, 0, 6, 1)
-# checkWinDown(table, 0, 0, 1)
+def isWin(funcs, table, col, row, player):
+    try:
+        for func in funcs:
+            if func(table, col, row, player):
+                return True
+        return False
+
+    except IndexError as e:
+        print(f'[ERROR] {e}')
+
+isWin(checkWinFuncs, table, 1, 4, 1)
