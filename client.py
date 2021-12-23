eb0fbdb37125d9502ab5f04ce1199d4e8756e7dd
Roy-Gal-Git <self.roygal@gmail.com>
2021-12-23 21:37:46 +0200

    Modify the file - printing 3 different stats at the end of the game

--- client.py
+++ client.py
@@ -11,6 +11,8 @@ ADDRESS = (HOST, PORT)
 
 def start_client():
     try:
+        start_time = time.time()
+
         client_socket.connect(ADDRESS)
 
         PLAYER = int(client_socket.recv(1024).decode(FORMAT))
@@ -44,13 +46,15 @@ def start_client():
             game = client_game(client_socket, PLAYER)
             print(f"\nResults:\n\
                 \nPlayer: {game['player']}\
-                \nServer: {game['server']}\
-                \nTurnes played: {game['turns']}")
+                \nServer: {game['server']}")
+                
             if game['server'] == game['total']:
+                print(f"Rounds: {game['server'] + game['player']}\nTurns: {game['turns']}\nTotal time played (Hours/Minutes/Seconds): {(time.time() - start_time) % 3600}::{(time.time() - start_time) % 60}::{time.time() - start_time}")
                 coloredPrint('\nYou lost the game! Good luck next time :)\n', MAGENTA)
                 break
             
             if game['player'] == game['total']:
+                print(f"Total rounds played: {game['server'] + game['player']}\nTotal turns played: {game['turns']}\nTotal time played (Hours/Minutes/Seconds): {round(time.time() - start_time) // 3600}::{round(time.time() - start_time) // 60}::{round(time.time() - start_time) % 60}")
                 coloredPrint('\nYou won the game! Hurray!\n', MAGENTA)
                 break
 
